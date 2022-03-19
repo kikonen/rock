2520074951a63d6d82b6b39b5b8f44bdc9937cda
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-03-19 12:31:38 +0200

    ADD RockAPI base

---
+++ rock-api/src/test/java/fi/ikari/rock/RockApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.ikari.rock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
