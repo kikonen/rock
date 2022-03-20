d07d41444df7310c2b15eb23960183aca89ad3e3
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-03-20 18:45:34 +0200

    FIX correct PUBLIC_URL & PORT path for ui build

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -14,9 +14,9 @@ echo "IMAGES: $BUILD_IMAGES"
 echo "$BUILD_IMAGES" | tr ' ' '\n' | while read IMAGE; do
     NAME=${BUILD_NAME}_${DOCKER_ENV}_${IMAGE}
 
-    echo "PUSH: $NAME"
+    echo "PUSH: ${DOCKER_REGISTRY_URL}/${NAME}:${BUILD_TAG}"
 
-    docker image push ${DOCKER_REGISTRY_URL}/${NAME}:latest
+    docker image push ${DOCKER_REGISTRY_URL}/${NAME}:${BUILD_TAG}
 done
 
 docker image ls | grep ${BUILD_NAME}_${DOCKER_ENV}
--- scripts/build_tag.sh
+++ scripts/build_tag.sh
@@ -14,7 +14,7 @@ echo "IMAGES: $BUILD_IMAGES"
 echo "$BUILD_IMAGES" | tr ' ' '\n' | while read IMAGE; do
     NAME=${BUILD_NAME}_${DOCKER_ENV}_${IMAGE}
 
-    echo "TAG: $NAME"
+    echo "TAG: ${DOCKER_REGISTRY_URL}/${NAME}:${BUILD_TAG}"
 
     docker image tag ${NAME} ${DOCKER_REGISTRY_URL}/${NAME}:latest
     docker image tag ${NAME} ${DOCKER_REGISTRY_URL}/${NAME}:${BUILD_TAG}
