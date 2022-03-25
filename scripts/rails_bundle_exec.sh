4c71fefb139f2d59ffe7312150dfbeeb2c4d1f05
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-03-25 23:14:24 +0200

    FIX check param

--- scripts/rails_bundle_exec.sh
+++ scripts/rails_bundle_exec.sh
@@ -3,6 +3,12 @@
 . $(realpath $(dirname $0))/base_env.sh
 
 CONTAINER=$1
+
+if [[ $CONTAINER == "" ]]; then
+    echo "USAGE: $0 [CONTAINER]"
+    exit
+fi
+
 SERVICE_MODE=debug $DOCKER_COMPOSE up -d $CONTAINER
 
 shift
