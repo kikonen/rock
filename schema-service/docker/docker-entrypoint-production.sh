d6895bf6799730c7269b8c97862eea5939ffb6f6
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-03-28 01:45:14 +0300

    FIX no need to keep master.key secret FIX CMD in schema prod

--- schema-service/docker/docker-entrypoint-production.sh
+++ schema-service/docker/docker-entrypoint-production.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-sudo chown -R docker:users /bundle /node_modules /app/log /app/tmp /app/public
-
-if [[ $MASTER_KEY != "" ]]; then
-    echo $MASTER_KEY > config/master.key
-    sudo chown docker:users config/master.key
+if [[ $1 != '' ]]; then
+    exec "$@"
+    exit $?
 fi
 
-echo "SERVER_MODE: $SERVER_MODE"
+echo "SERVICE_MODE: $SERVICE_MODE"
 
-if [[ "$SERVER_MODE" == "debug" ]]; then
+if [[ "$SERVICE_MODE" == "debug" ]]; then
     sleep infinity
 else
-    # NOTE KI *NEW* intance clear old trash
-    if [[ -f /app/tmp/pids/server.pid ]]; then
-        rm /app/tmp/pids/server.pid
-    fi
-
-    bundle exec rails s -b 0.0.0.0 -p 3000
+    #rake db:drop db:create db:migrate db:setup
+    rake db:create
+    rake db:migrate
 fi
