b6850e55c8308382ac8d591a2e099fc62f798e6e
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-04-04 19:07:35 +0300

    FIX pass args

--- scripts/production_build.sh
+++ scripts/production_build.sh
@@ -6,5 +6,8 @@ export DOCKER_ENV=build
 $SCRIPT_DIR/setup_build_info.sh
 
 time $DOCKER_COMPOSE build \
+     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
+     --build-arg BUILD_NAME=$BUILD_NAME \
+     --build-arg BASE_TAG=$BASE_TAG \
      --build-arg DOCKER_UID=$DOCKER_UID \
      --build-arg DOCKER_GID=$DOCKER_GID "$@"
